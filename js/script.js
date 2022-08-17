b12ab0459d316cb75e8939659c502f85c7a30a6b
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-08-17 09:17:17 +0200

    ostateczne

--- js/script.js
+++ js/script.js
@@ -1,85 +1,85 @@
 let form = document.getElementById('form');
-let errors = document.getElementById('error');
-errors.innerHTML = '';
 let acceptAll = document.getElementById('zgodyAll');
-let acceptOne = document.getElementById('zmOne');
-let acceptTwo = document.getElementById('zmTwo');
 let button = document.querySelector('button');
-
-
 
 const validate = (event) => {
 
-
-
-
-
-    event.preventDefault();
-}
-
-
-const checkInput = (event) => {
-
-    // const checkInput = (event) => {
-
-    //     let fullName = document.getElementById('flname');
-    //     let errors = document.getElementById('error');
-    //     errors.innerHTML = '';
-    
-    //     if (fullName.value.trim() === '') {
-    //         let li = document.createElement('li');
-    //         li.innerText = 'Wprowadz poprawne dane';
-    //         errors.appendChild(li);
-    //     }
-        
-    //     event.preventDefault();
-        
-    // }
-
     let fullName = document.getElementById('flname');
-    
+    let email = document.getElementById('email');
+    let errors = document.getElementById('error');
+    let acceptOne = document.getElementById('zmOne');
 
-    let value = fullName.value;
-    let trim = value.trim();
+    errors.innerHTML = '';
 
-    if (!trim) {
+    let valueName = fullName.value;
+    let trimName = valueName.trim();
+
+    let valueEmail = email.value;
+    let trimEmail = valueEmail.trim();
+
+
+    if (!trimName) {
         let li = document.createElement('li');
         li.innerText = 'Wprowadz poprawne dane';
         errors.appendChild(li);
     }
-    event.preventDefault();
-    
-}
 
-
-function checkEmail(event) {
-    let email = document.getElementById('email');
-
-    let value = email.value;
-    
-    let trim = value.trim();
-
-    if (!trim) {
+    if (!trimEmail) {
         let li = document.createElement('li');
         li.innerText = 'Wprowadz poprawny adres email';
         errors.appendChild(li);
     }
 
-    // if (include) {
-    //     let li = document.createElement('li');
-    //     li.innerText = 'Adres email powinien zawierać @';
-    //     errors.appendChild(li);
-    // }
+    if (!email.value.includes('@')) {
+        let li = document.createElement('li');
+        li.innerText = 'Adres email powinien zawierać @';
+        errors.appendChild(li);
+    }
 
+    if (!acceptOne.checked) {
+        let li = document.createElement('li');
+        li.innerText = 'Nie zaznaczono zgody marketingowej 1';
+        errors.appendChild(li);
+    }
     event.preventDefault();
 }
 
+const checkAll = () => {
+    
+    let checkOne = document.getElementById('zmOne');
+    let checkTwo = document.getElementById('zmTwo');
+
+    if(acceptAll.checked) {
+        checkOne.checked = true;
+        checkTwo.checked = true;
+        checkOne.disabled = true;
+        checkTwo.disabled = true;
+    }
+
+    if(acceptAll.checked == false) {
+        checkOne.checked = false;
+        checkTwo.checked = false;
+        checkOne.disabled = false;
+        checkTwo.disabled = false;
+    }
+
+}
+
+// const Agree = (event) => {
+//     let checkOne = document.getElementById('zmOne');
+//     let checkTwo = document.getElementById('zmTwo');
+
+//     checkOne.checked = event.target.checked;
+//     checkTwo.checked = event.target.checked;
+
+//     checkOne.disabled = event.target.checked;
+//     checkTwo.disabled = event.target.checked;
+// }
 
 
 
 form.addEventListener('submit', validate);
-form.addEventListener('submit', checkInput);
-form.addEventListener('submit', checkEmail);
+acceptAll.addEventListener('change', checkAll);
 
 
 
